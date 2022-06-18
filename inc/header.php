4d827d7f05d63bb4c14c3ae7572edd3e4f2a7bbe
Oke <56229919+onissen@users.noreply.github.com>
2022-06-18 12:44:03 +0200

    Initial Code

---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://northware-cockpit.test/css/cockpit_main.css">
+    <title>Northware Cockpit</title>
+</head>
+<body>
