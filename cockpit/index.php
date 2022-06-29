7390838fc5edeff3b71dc8d6abd950bf7f517f01
Oke <56229919+onissen@users.noreply.github.com>
2022-06-29 18:00:48 +0200

    Fixed Navbar-Bug COckpit-Home and added index.php to /cockpit to avoid problems

---
+++ cockpit/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0 URL=../">
+    <title>Weiterleitung</title>
+</head>
+<body>
+    Diese Seite sollte Sie sofort weiterleiten. Ansonsten klicken Sie bitte <a href="../">hier</a> und benachrichtigen die IT-Abteilung.
+</body>
+</html>
--- components/navbar-contents.php
+++ components/navbar-contents.php
@@ -1,7 +1,7 @@
 <?php if ($service == 'cockpit') { ?>
     <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
-            <a href="http://northware-cockpit.test/cockpit" class="nav-link">Cockpit Home</a>
+            <a href="http://northware-cockpit.test/" class="nav-link">Cockpit Home</a>
         </li>
         <li class="nav-item">
             <a href="http://northware-cockpit.test/cockpit/admin" class="nav-link">Administration</a>
